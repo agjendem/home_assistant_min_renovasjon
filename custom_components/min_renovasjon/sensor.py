e5d8dad390d0eb3a8a0bcf802d6e97bdf09ccb97
Anders Gjendem <anders.gjendem@gmail.com>
2023-04-20 09:50:26 +0200

    WIP: Add binary sensor for waste collection today & tomorrow

--- custom_components/min_renovasjon/sensor.py
+++ custom_components/min_renovasjon/sensor.py
@@ -1,7 +1,8 @@
-from datetime import timedelta
+from datetime import timedelta, datetime, date
 import logging
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
@@ -26,6 +27,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     add_entities(
         MinRenovasjonSensor(min_renovasjon, fraction_id) for fraction_id in fraction_ids
+    )
+    add_entities(
+        MinRenovasjonWasteCollectionDaySensor(min_renovasjon, fraction_id) for fraction_id in fraction_ids
+    )
+    add_entities(
+        MinRenovasjonWasteCollectionDayTomorrowSensor(min_renovasjon, fraction_id) for fraction_id in fraction_ids
     )
 
 
@@ -59,3 +66,66 @@ class MinRenovasjonSensor(Entity):
     def update(self):
         """Update calendar."""
         self._min_renovasjon.refresh_calendar()
+
+
+class MinRenovasjonWasteCollectionDaySensor(BinarySensorEntity):
+    """Representation of a BinarySensor for waste collection day."""
+    _attr_has_entity_name = True
+
+    def __init__(self, min_renovasjon, fraction_id):
+        """Initialize with API object, device id."""
+        self._min_renovasjon = min_renovasjon
+        self._fraction_id = fraction_id
+
+    @property
+    def name(self):
+        """Return the name of the fraction if any."""
+        fraction = self._min_renovasjon.get_calender_for_fraction(self._fraction_id)
+        if fraction is not None:
+            return f'{fraction[1]}_waste_collection_day_today'
+
+    @property
+    def is_on(self) -> bool | None:
+        fraction = self._min_renovasjon.get_calender_for_fraction(self._fraction_id)
+        if fraction is not None:
+            first_waste_collection_day = fraction[3]
+            second_waste_collection_day = fraction[4]
+            trigger_date = self._get_trigger_date()
+
+            return self._is_same_date(trigger_date, first_waste_collection_day) \
+                or self._is_same_date(trigger_date, second_waste_collection_day)
+
+    @staticmethod
+    def _is_same_date(as_date: date, waste_collection_date: datetime | None) -> bool | None:
+        if waste_collection_date:
+            return waste_collection_date.date() == as_date
+
+    @staticmethod
+    def _get_trigger_date() -> date:
+        return datetime.today().date()
+
+    @property
+    def entity_picture(self):
+        """Symbol."""
+        fraction = self._min_renovasjon.get_calender_for_fraction(self._fraction_id)
+        if fraction is not None:
+            return fraction[2]
+
+    def update(self):
+        """Update calendar."""
+        self._min_renovasjon.refresh_calendar()
+
+
+class MinRenovasjonWasteCollectionDayTomorrowSensor(MinRenovasjonWasteCollectionDaySensor):
+
+    @property
+    def name(self):
+        """Return the name of the fraction if any."""
+        fraction = self._min_renovasjon.get_calender_for_fraction(self._fraction_id)
+        if fraction is not None:
+            return f'{fraction[1]}_waste_collection_day_tomorrow'
+
+    @staticmethod
+    def _get_trigger_date() -> date:
+        tomorrow = datetime.today() + timedelta(days=1)
+        return tomorrow.date()
